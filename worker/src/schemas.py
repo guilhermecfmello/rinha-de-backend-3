434f3cc3d0a1662948dedf271ee72b5f45ab83e1
Guilherme Mello <guilhermecfmello@gmail.com>
2025-07-23 17:29:27 -0300

    Fix schema bugs

--- worker/src/schemas.py
+++ worker/src/schemas.py
@@ -14,3 +14,7 @@ class PaymentProcessorRequest(BaseModel):
     correlation_id: UUID = Field(..., alias="correlationId")
     amount: float = Field(..., description="Amount to be processed")
     requested_at: str = Field(..., alias="requestedAt", description="Timestamp when the request was made")
+    
+    model_config = {
+        "populate_by_name": True
+    }
--- worker/src/services/worker_service.py
+++ worker/src/services/worker_service.py
@@ -3,7 +3,7 @@ import redis
 import json
 
 from config import Config
-from schemas import PaymentProcessorRequest
+from src.schemas import PaymentProcessorRequest
 from src.services.payment_processor_service import PaymentProcessorService
 
 logger = logging.getLogger(__name__)
@@ -26,9 +26,9 @@ class WorkerService:
             amount = data["amount"]
             requested_at = data["requestedAt"]
             payment_request = PaymentProcessorRequest(
-                correlationId=correlation_id,
+                correlation_id=correlation_id,
                 amount=amount,
-                requestedAt=requested_at
+                requested_at=requested_at
             )
             self.payment_processor_service.process_payment(payment_request)
             
