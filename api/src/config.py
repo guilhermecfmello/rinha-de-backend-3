d7f53221616e077b0029211d338750795da7cae2
Guilherme Mello <guilhermecfmello@gmail.com>
2025-07-23 16:13:49 -0300

    Add request queue name config to project

--- api/src/config.py
+++ api/src/config.py
@@ -3,3 +3,4 @@ import os
 
 class Config:
     REDIS_URL = os.getenv("REDIS_URL")
+    PAYMENT_REQUEST_QUEUE = os.getenv("PAYMENT_REQUEST_QUEUE")
--- api/src/services/payment_service.py
+++ api/src/services/payment_service.py
@@ -3,13 +3,15 @@ from uuid import UUID
 
 import redis
 
+from src.config import Config
 from src.schemas import PaymentRequest
 from src.models import PaymentMessageRequest
 
 
 class PaymentService:
-    def __init__(self, redis_url="redis://localhost:6379/0"):
-        self.redis_client = redis.Redis.from_url(redis_url)
+    def __init__(self):
+        self.redis_client = redis.Redis.from_url(Config.REDIS_URL)
+        self.redis_request_requeue_name = Config.PAYMENT_REQUEST_QUEUE
 
     def create_payment(self, payment_request: PaymentRequest) -> UUID:
         requested_at = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + "Z"
@@ -18,5 +20,5 @@ class PaymentService:
             amount=payment_request.amount,
             requested_at=requested_at
         )
-        self.redis_client.lpush("payments_request_queue", payment_message_request.model_dump_json(by_alias=True))
+        self.redis_client.lpush(self.redis_request_requeue_name, payment_message_request.model_dump_json(by_alias=True))
         return payment_request.correlation_id
