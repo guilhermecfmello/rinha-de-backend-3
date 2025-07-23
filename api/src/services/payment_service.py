640188143b0c74f693421da8b2032887fe9c7d40
Guilherme Mello <guilhermecfmello@gmail.com>
2025-07-23 16:36:12 -0300

    add logs to track payment service performance.

--- api/src/services/payment_service.py
+++ api/src/services/payment_service.py
@@ -16,11 +16,16 @@ class PaymentService:
         self.redis_request_requeue_name = Config.PAYMENT_REQUEST_QUEUE
 
     def create_payment(self, payment_request: PaymentRequest) -> UUID:
-        requested_at = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + "Z"
+        start_time = datetime.now(timezone.utc)
+        requested_at = start_time.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + "Z"
         payment_message_request = PaymentMessageRequest(
             correlation_id=payment_request.correlation_id,
             amount=payment_request.amount,
             requested_at=requested_at
         )
+        logger.info("Posting message to Redis queue...")
         self.redis_client.lpush(self.redis_request_requeue_name, payment_message_request.model_dump_json(by_alias=True))
+        end_time = datetime.now(timezone.utc)
+        elapsed_ms = (end_time - start_time).total_seconds() * 1000
+        logger.info(f"Posted message to queue in {elapsed_ms:.2f} ms")
         return payment_request.correlation_id
