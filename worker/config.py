38806d0f0000994a7b942e907f29b5648c722357
Guilherme Mello <guilhermecfmello@gmail.com>
2025-07-24 16:42:08 -0300

    Add token header config

--- worker/config.py
+++ worker/config.py
@@ -3,6 +3,7 @@ import os
 class Config:
     LOG_LEVEL = os.getenv("LOG_LEVEL")
     REDIS_URL = os.getenv("REDIS_URL")
+    RINHA_TOKEN = os.getenv("RINHA_TOKEN")
     PAYMENT_REQUEST_QUEUE = os.getenv("PAYMENT_REQUEST_QUEUE")
     PROCESSOR_DEFAULT_URL = os.getenv("PROCESSOR_DEFAULT_URL")
     PROCESSOR_FALLBACK_URL = os.getenv("PROCESSOR_FALLBACK_URL")
